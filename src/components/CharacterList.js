d92186d2ad9cb4e036e1d3cea4772f95bd23cf2b
bigtonito39 <bigtonito@hotmail.com>
2019-11-10 21:56:50 -0500

    Project pretty much done, just need to add a few touches

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,61 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from 'styled-components'
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters,setCharacters] = useState("")
+
+const CardsSection = styled.section`
+display:flex;
+flex-wrap:wrap;
+width:100%;
+flex-direction:column;
+flex-basis:auto;
+
+`;
+const Cards = styled.div`
+border:dotted gold 5px;
+margin:2%;
+width:50%;
+background-image: url("https://owips.com/sites/default/files/clipart/rick-and-morty-clipart/217071/rick-and-morty-clipart-white-background-217071-7825200.jpg");
+padding:5%;
+margin-left:20%;
+
+`;
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://rickandmortyapi.com/api/character/")
+    .then((res)=>{
+      setCharacters(res.data.results)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
   }, []);
+console.log(characters)
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+    <CardsSection className="character-list">
+                   
+      {characters && characters.map(char =>(
+      <Cards key={char.id}>
+      {console.log(char)}
+   <h1>{char.name}</h1>
+     
+      <h3>{`a/an ${char.species} thats ${char.status}`}</h3>
+      <img 
+      src={char.image}
+      alt={char.name}
+           />  
+      </Cards>
+      ))}
+ 
+    </CardsSection>
+    
+    
+    );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,17 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const [searchWord,setsearchWord]=useState("");
+  const [results,SetResults]=useState([]);
+
+  const trackChanges = event => {
+    setsearchWord(event.target.value)
+  }
+  
+  useEffect(()=> {
+    const results = ""
+  })
+
  
   return (
     <section className="search-form">
