929974355af14d4a53e3c3a80b19c65206bdb37f
bigtonito39 <bigtonito@hotmail.com>
2019-11-11 19:08:08 -0500

    Attempting to fix search field bug

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 
 export default function SearchForm(props) {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   
+   
  
     const handleChange = e => {
-      setSearchTerm(e.target.value);
+      setSearchTerm({...searchTerm,[e.target.name]:e.target.value});
       const results = props.characters.filter(character => 
         character.name.toLowerCase().includes(e.target.value)
-          
-        
+         
+      
       );
-  
+      console.log(e)
       setSearchResults(results);
       props.setCharacters(results);
-      
+     
     };
     
  
   return (
     <section className="search-form">
-      <form>
+      <form  >
       <input
         type="search"
         placeholder="Search By Name"
         value={searchTerm}
         onChange={handleChange}
-        
+                
       />
+      <button type="submit">Submit</button>
       </form>
      
     </section>
